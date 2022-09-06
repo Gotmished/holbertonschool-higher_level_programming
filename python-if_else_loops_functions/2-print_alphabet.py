f8225725d54e5440a3d5330521588ae2054ce7f4
Gotmished <5002@holbertonschool.com>
2022-09-05 20:41:10 -0700

    removed comments

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-# result = ""
-#for i in range(97, 123):
-#    result = result + chr(i)
-#
-#print(result)
-
 for i in range(ord('a'), ord('z') + 1):
     print("{:c}".format(i), end="")
