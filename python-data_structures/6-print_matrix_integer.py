dddef9446e40c128790cc3297814dead71916836
Gotmished <5002@holbertonschool.com>
2022-09-12 00:32:47 -0700

    changed counting range

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             print("{:d}".format(matrix[i][j]), end='')
-            if j == range(len(matrix[0])):
+            if j == len(matrix[0]):
                 continue
             print(" ", end='')
         print()
