2cbd7c6c291f4d333f1cde789f13a79b70e7338e
Gotmished <5002@holbertonschool.com>
2022-09-12 00:30:37 -0700

    Addressing spaces again

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     x = ' '
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            print("{:d} ".format(matrix[i][j]), end='')
-        print('\r', end='')
+            print("{:d}".format(matrix[i][j]), end='')
+            if j == range(len(matrix[0])):
+                continue
+            print(" ", end='')
         print()
