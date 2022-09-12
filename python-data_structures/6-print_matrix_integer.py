50af4f3b951676e7954c773062ef5f386fd148d9
Gotmished <5002@holbertonschool.com>
2022-09-12 00:36:39 -0700

    Re-worked order of operations

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             print("{:d}".format(matrix[i][j]), end='')
-            if j != len(matrix[0]):
+            if j < len(matrix[0]):
                 print(" ", end='')
         print()
