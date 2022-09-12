c5aa45b5f317e19f940be2d7587626c67a402e88
Gotmished <5002@holbertonschool.com>
2022-09-12 00:25:23 -0700

    Another attempt to print correct number of spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
+    x = ' '
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            if j == len(matrix[0]):
-                print("{:d}".format(matrix[i][j]), end='')
-            else:
-                print("{:d} ".format(matrix[i][j]), end='')
+            print("{:d} ".format(matrix[i][j]), end='')
+        print('\r', end='')
         print()
