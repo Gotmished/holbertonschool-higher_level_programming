f465aa90fc6f59ed9292d5688684f064350a3b24
Gotmished <5002@holbertonschool.com>
2022-09-12 17:23:00 -0700

    Removed superfluous code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    x = ' '
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             print("{:d}".format(matrix[i][j]), end='')
