cf11c535acf30622507ea8471cf40e27c007ba02
Gotmished <5002@holbertonschool.com>
2022-09-18 02:08:40 -0700

    Removal of surplus spaces

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
 
     if a_dictionary:
-            return (list(sorted(a_dictionary.keys(), reverse = True))[0])
+            return (list(sorted(a_dictionary.keys(), reverse=True))[0])
     else:
         return None
