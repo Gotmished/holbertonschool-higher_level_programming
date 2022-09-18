802102655ddcc29ab1b72c192b3ca9dc8c171165
Gotmished <5002@holbertonschool.com>
2022-09-18 02:01:02 -0700

    Added None return

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,7 @@
 def best_score(a_dictionary):
 
     if a_dictionary is not None:
-        return(list(sorted(a_dictionary.keys(), reverse = True))[0])
+        for key in sorted(a_dictionary.keys(), reverse=True):
+            return key
+    else:
+        return None
