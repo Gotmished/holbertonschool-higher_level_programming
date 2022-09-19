fcb96814ef2bc08cec8a7aabad2f0a36c448de7b
Gotmished <5002@holbertonschool.com>
2022-09-19 03:56:30 -0700

    Initial commit task to define a class for a square using a property getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class represents the Square with a certain size"""
+    def __init__(self, size=0):
+            self.__size = size
+
+    @property
+    def size(self):
+        """Property - returns __size of square"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Property setter - of __size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public instance method - returns the area of the Square"""
+        return(self.__size * self.__size)
