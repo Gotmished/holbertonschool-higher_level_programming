02a83fd145a9f340af9284ab61b2d299f592c5af
Gotmished <5002@holbertonschool.com>
2022-09-19 02:45:06 -0700

    Initial commit task to define a class for a square and return the area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class represents the Square with a certain size"""
+    def __init__(self, size=0):
+        """Initialises the Square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the Square"""
+        return(self.__size * self.__size)
