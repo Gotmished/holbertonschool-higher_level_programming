daa99119af143bc5fe33d018b5aebca70e5f3c02
Gotmished <5002@holbertonschool.com>
2022-09-20 01:03:33 -0700

    Reordered functions following EUREKA! moment of how this whole things works

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class represents the Square with a certain size"""
     def __init__(self, size=0):
-            self.__size = size
+            self.size = size
 
     @property
     def size(self):
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,23 +3,10 @@
 
 
 class Square:
-    """Class represents the Square with a certain size"""
+    """Class represents a square with a certain size and position in space"""
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-        if type(position) != tuple or len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(position[0]) != int or type(position[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+            self.size = size
+            self.position = position
 
     @property
     def size(self):
@@ -29,17 +16,29 @@ class Square:
     @size.setter
     def size(self, value):
         """Property setter - of __size"""
-        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        """Property setter - returns __position of square"""
+        """Property setter - returns __position of square in (x, y)"""
         return(self.__position)
 
     @position.setter
     def position(self, value):
         """Property setter - of __property"""
-        self.__position = value
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
         """Public instance method - returns the area of the Square"""
