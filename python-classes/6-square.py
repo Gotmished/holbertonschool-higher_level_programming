aedef982971077584530a8552c0be4e6194eff10
Gotmished <5002@holbertonschool.com>
2022-09-20 00:40:02 -0700

    Fixed error messages - now occur in correct places

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,7 +5,20 @@
 class Square:
     """Class represents the Square with a certain size"""
     def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
+
+        if type(position) != tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) != int or type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
             self.__position = position
 
     @property
@@ -16,12 +29,7 @@ class Square:
     @size.setter
     def size(self, value):
         """Property setter - of __size"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
@@ -31,14 +39,7 @@ class Square:
     @position.setter
     def position(self, value):
         """Property setter - of __property"""
-        if not isinstance(value, tuple) or len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(value[0], int) or isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """Public instance method - returns the area of the Square"""
