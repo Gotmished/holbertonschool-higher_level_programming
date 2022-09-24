471adbe9e2a2bfe3c3c15fdbe24dfe60e3ecf542
Gotmished <5002@holbertonschool.com>
2022-09-24 00:06:09 -0700

    Shortened three lines to adhere to style convention

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,15 +11,18 @@ def matrix_divided(matrix, div):
     Divides each element of a matrix by a number and returns the new matrix
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = []
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         for element in row:
             if not isinstance(element, int) and not isinstance(element, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
         row_length.append(len(row))
         if not all(len_element == row_length[0] for len_element in row_length):
             raise TypeError("Each row of the matrix must have the same size")
