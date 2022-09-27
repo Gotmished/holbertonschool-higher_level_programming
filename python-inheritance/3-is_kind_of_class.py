1e3c44ed64edde246db33a5286500440e15c86ea
Gotmished <5002@holbertonschool.com>
2022-09-27 00:40:28 -0700

    Initial commit task three - more instances

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module contains the is_kind_of_class function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True or False depending upon whether object is
+    an instance of a class, or an instance of a class inherited
+    from a specific class
+    """
+    return (isinstance(obj, a_class))
