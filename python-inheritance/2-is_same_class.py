3073e6325a07ebae6753bdff7c388fb24acdda70
Gotmished <5002@holbertonschool.com>
2022-09-27 00:35:48 -0700

    Removed unnecessary bracket

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,7 +6,7 @@ This module contains the is_same_class function
 
 def is_same_class(obj, a_class):
     """
-    Returns True if obj is an instance of specified class, exactly.
-    False, otherwise.
+    Returns True if obj is an instance of specified class, exactly,
+    or false, otherwise.
     """
-    return (type(obj) is a_class))
+    return (type(obj) is a_class)
