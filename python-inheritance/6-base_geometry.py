4fe69e49b533faf632c8d8e55e79d1a63418277b
Gotmished <5002@holbertonschool.com>
2022-09-27 05:02:15 -0700

    Initial commit task six - a class with a method that raises an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module "5-base_geometry" including "BaseGeometry" class
+and public instance method "area"
+"""
+
+
+class BaseGeometry:
+    """
+    A class with public instance method "area"
+    """
+    def area(self):
+        """
+        Raises an exception for area
+        """
+        raise Exception("area() is not implemented")
