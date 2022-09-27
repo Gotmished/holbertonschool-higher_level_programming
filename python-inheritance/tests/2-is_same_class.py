5e30c1d1f44c558cb4010e959d88019e5e3e1745
Gotmished <5002@holbertonschool.com>
2022-09-27 00:26:17 -0700

    Initial commit task two - is object an instance of a particular class

---
+++ python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The "is_same_class" function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if obj is an instance of specified class, exactly.
+    False, otherwise.
+    """
+    return (type(obj) is a_class)
