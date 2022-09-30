a8c4a6789835baf9b7e2461e8403183c32a13cd8
Gotmished <5002@holbertonschool.com>
2022-09-29 19:32:00 -0700

    Initial commit task one - write text to a file, and return char count

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module containing "write_file" function
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string "text" to a file "filename" and
+    returns the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        filename = f.write(text)
+        return (len(text))
