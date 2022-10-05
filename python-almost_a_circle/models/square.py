51ad5dcd1d594e128d5326219802086f48fabcb9
Gotmished <5002@holbertonschool.com>
2022-10-04 22:31:53 -0700

    Initial commit task ten - Create Square class that inherits from Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module containing "Square" subclass that inherits
+from "Rectangle" subclass of "Base" class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class that represents a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor for instantiation of a rectangle
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """
+        String representation of a square
+        """
+        return(f"[Square] ({self.id}) {self.x}/\
+{self.y} - {self.height}")
