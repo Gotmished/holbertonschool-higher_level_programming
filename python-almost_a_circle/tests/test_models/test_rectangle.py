4665044630b32f61465875771217ee0d892060c2
Gotmished <5002@holbertonschool.com>
2022-10-07 04:35:56 -0700

    Added more tests - Rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,6 +3,9 @@
 Contains tests for Rectangle subclass of Base class
 """
 
+import os
+import json
+import pycodestyle
 import inspect
 import unittest
 from models import rectangle
@@ -29,11 +32,24 @@ class TestDocsRectangle(unittest.TestCase):
     def test_func_docstrings(self):
         """Tests for the presence of documentation in all functions"""
         for func in self.rectangle_funcs:
-            if not func[1].__doc__:
-                print(f"{func[0]} lacks documentation")
-            else:
-                self.assertTrue(len(func[1].__doc__) >= 1)
+            self.assertTrue(len(func[1].__doc__) >= 1)
+#            if not func[1].__doc__:
+#                print(f"{func[0]} lacks documentation")
+#            else:
 
+    def test_pycode_class(self):
+        """ Checks pycodestyle for base """
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pycode_test(self):
+        """ Checks pycodestyle for test_base """
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 class TestRectangle(unittest.TestCase):
     """Tests class functionality"""
@@ -88,11 +104,15 @@ class TestRectangle(unittest.TestCase):
         """Test handling ints <= 0 for width"""
         with self.assertRaises(ValueError):
             Rectangle(-1, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 1)
 
     def test_height_non_positive(self):
         """Test handling ints <= 0 for height"""
         with self.assertRaises(ValueError):
             Rectangle(1, -1)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
 
     def test_non_coordinate_x(self):
         """Tests for handling of non-coordinate"""
@@ -123,7 +143,92 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.r2.area(1)
 
+    def test___str___exists(self):
+        """Testing normal functioning of __str__"""
+        self.assertEqual(self.r5.__str__(), '[Rectangle] (14) 12/13 - 10/11')
+
+    def test_display(self):
+        """Testing normal functioning of display"""
+        self.assertTrue(isinstance(self.r2.display(), str))
+
     def test_display_too_many_args(self):
         """Test handling display with too many args"""
         with self.assertRaises(TypeError):
             self.r2.display(1)
+
+    def test_to_dictionary(self):
+        """Testing normal functioning of to_dictionary"""
+        self.assertTrue(isinstance(self.r5.to_dictionary(), dict))
+
+    def test_update_args(self):
+        """Testing normal functioning of update with *args"""
+        r = Rectangle(1, 1, 0, 0, 1)
+        self.assertEqual(str(r), "[Rectangle] (1) 0/0 - 1/1")
+        r.update(89)
+        self.assertEqual(str(r), "[Rectangle] (89) 0/0 - 1/1")
+        r.update(89, 2)
+        self.assertEqual(str(r), "[Rectangle] (89) 0/0 - 2/1")
+        r.update(89, 2, 3)
+        self.assertEqual(str(r), "[Rectangle] (89) 0/0 - 2/3")
+        r.update(89, 2, 3, 4)
+        self.assertEqual(str(r), "[Rectangle] (89) 4/0 - 2/3")
+        r.update(89, 2, 3, 4, 5)
+        self.assertEqual(str(r), "[Rectangle] (89) 4/5 - 2/3")
+
+    def test_update_kwargs(self):
+        """Testing normal functioning of update with **kwargs"""
+        r = Rectangle(1, 1, 0, 0, 1)
+        self.assertEqual(str(r), "[Rectangle] (1) 0/0 - 1/1")
+        r.update(height=10)
+        self.assertEqual(str(r), "[Rectangle] (1) 0/0 - 1/10")
+        r.update(width=11, x=2)
+        self.assertEqual(str(r), "[Rectangle] (1) 2/0 - 11/10")
+        r.update(y=3, width=4, x=5, id=89)
+        self.assertEqual(str(r), "[Rectangle] (89) 5/3 - 4/10")
+        r.update(x=6, height=7, y=8, width=9)
+        self.assertEqual(str(r), "[Rectangle] (89) 6/8 - 9/7")
+
+    def test_create(self):
+        """Testing normal functioning of create"""
+        r = {"id": 89, "width": 1, "height": 2, "x": 3, "y": 4}
+        rc = Rectangle.create(**r)
+        self.assertEqual("[Rectangle] (89) 3/4 - 1/2", str(rc))
+
+    def test_None_save_to_file(self):
+        """Testing None with save_to_file"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r", encoding='utf-8') as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_empty_save_to_file(self):
+        """Testing empty list with save_to_file"""
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_normal_save_to_file(self):
+        """Testing normal functioning of save_to_file with two instances"""
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        l_insts = [r1, r2]
+        Rectangle.save_to_file(l_insts)
+        with open("Rectangle.json", "r") as f:
+            l_dicts = [r1.to_dictionary(), r2.to_dictionary()]
+            self.assertEqual(json.dumps(l_dicts), f.read())
+
+    def test_load_from_file(self):
+        """Testing load_from_file"""
+        l_insts = [self.r2, self.r3]
+        Rectangle.save_to_file(l_insts)
+        l_from = Rectangle.load_from_file()
+        self.assertEqual(str(l_from[1]), str(self.r3))
+
+    def test_load_from_file_no_file(self):
+        """Testing load_from_file when file doesn't exist"""
+        filename = "Rectangle.json"
+        if os.path.exists(filename):
+            os.remove(filename)
+        self.assertEqual(Rectangle.load_from_file(), [])
+
+if __name__ == "__main__":
+    unittest.main()
