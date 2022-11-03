e28eceed03aafb6c77de0afffee7c9412604ca73
Gotmished <5002@holbertonschool.com>
2022-11-03 00:34:04 -0700

    Re-wrote solution - contained errors

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,9 @@
 -- Task two: creating a database and a user with SELECT privileges only
+-- for all tables within the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2)pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2)pwd';
 
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 FLUSH PRIVILEGES;
