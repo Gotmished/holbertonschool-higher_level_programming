05a2fa4557b89503849ea3ea0516a8da3c403e42
Gotmished <5002@holbertonschool.com>
2022-11-03 04:46:05 -0700

    Initial commit task seven - Foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Task seven: creating a database and adding a table.
+-- use of primary keys, auto generation, and foreign keys
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id)
+);
