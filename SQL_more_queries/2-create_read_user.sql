0513a5fb15fc15fc57bc834ce83d60546b4b1edc
Gotmished <5002@holbertonschool.com>
2022-11-03 00:44:28 -0700

    Corrected password

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- for all tables within the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2)pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
