475de5fda6579eacd0b2fd33be0456b2e550b333
Gotmished <5002@holbertonschool.com>
2022-11-02 18:45:06 -0700

    Included conversion for first_table and first_table.name

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Converts a database to UTF-8
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- Converts a database, first_table, and first_table.name to UTF-8
+USE hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
