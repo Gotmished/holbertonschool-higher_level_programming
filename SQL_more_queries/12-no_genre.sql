b6e11a6fd4456d252e53a4b95e67aa5f22b093bb
Gotmished <5002@holbertonschool.com>
2022-11-06 02:28:30 -0800

    changed joining ON columns

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -4,6 +4,6 @@ SELECT
 	tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY title, genre_id;
