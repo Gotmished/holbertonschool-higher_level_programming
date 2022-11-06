0f2128150feb969d82a2c7b85b1973020eb503f4
Gotmished <5002@holbertonschool.com>
2022-11-06 01:03:13 -0800

    Initial commit task twelve - Left join with NULL property

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Task twelve: List all television shows without a genre
+SELECT
+	tv_shows.title,
+	tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY title, genre_id;
