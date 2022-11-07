59172d394ac4e57856d8ff621c16319590757439
Gotmished <5002@holbertonschool.com>
2022-11-07 02:05:01 -0800

    Initial commit task two - returning a row based upon user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module to display values in states table where
+name corresponds to a partiular argument
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Returns values ordered by ID"""
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect("localhost", username, password, database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states \
+    WHERE name = '{}' \
+    ORDER BY id ASC".format(state_name))
+    state_rows = cursor.fetchall()
+    for row in state_rows:
+        print(row)
