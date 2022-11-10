9d13aa5374978f879eed7394efb7e73b3d657af1
Gotmished <5002@holbertonschool.com>
2022-11-10 02:31:54 -0800

    Initial commit task thirteen - deleting entries based upon a criterion

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module to delete all State objects
+containing the letter 'a' from hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Link class to table in database"""
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session.configure(bind=engine)
+    session = Session()
+
+    try:
+        for state in session.query(State).filter(State.name.contains('a')):
+            session.delete(state)
+    except Exception:
+        session.rollback()
+        raise
+    else:
+        session.commit()
+
+    session.close()
