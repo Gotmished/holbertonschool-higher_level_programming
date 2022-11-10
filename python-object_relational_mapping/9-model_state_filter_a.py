c9766a41dc59e44a734fd8a1f88ae91bfd139c6f
Gotmished <5002@holbertonschool.com>
2022-11-09 20:34:57 -0800

    Initial commit task nine - using filter() method to find records containing a particular char

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module to list all State objects in hbtn_0e_6_usa
+containing the letter a
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Link class to table in database"""
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session.configure(bind=engine)
+    session = Session()
+
+    for states in session.query(State).filter(State.name.contains('a')):
+        print(f"{states.id}: {states.name}")
