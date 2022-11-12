c36dbb09557edf8d236c0a326592bca3ccf3c517
Gotmished <5002@holbertonschool.com>
2022-11-12 03:21:13 -0800

    Editing for pycodestyle

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State).\
-        filter(City.state_id == State.id).order_by(City.id):
+    for city, state in session.query(City, State).
+    filter(City.state_id == State.id).order_by(City.id):
         print(f"{state.name}: ({city.id}) {city.name}")
