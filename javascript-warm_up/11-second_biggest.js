bf90378c3f03db96fd05bd4c65fd91ab5f15cb3b
Gotmished <5002@holbertonstudents.com>
2022-12-10 23:09:12 +1100

    Now using function within sort() to reverse order of array

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const data = process.argv.splice(2);
 if (data.length < 2) {
   console.log('0');
 } else {
-  data.reverse();
+  data.sort(function (a, b) { return b - a; });
   console.log(data[1]);
 }
