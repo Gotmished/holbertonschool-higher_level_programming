c7af00919140f2c2988278ab0b2d0a791224c635
Gotmished <5002@holbertonstudents.com>
2022-12-12 21:44:52 +1100

    Initial commit task three - using argv to get a particular piece of information from a website

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+request
+  .get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(JSON.parse(body).title);
+  });
