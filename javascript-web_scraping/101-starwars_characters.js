8424d38c099a8a87e3b690f7d9bdac034acd7275
Gotmished <5002@holbertonstudents.com>
2022-12-13 18:06:26 +1100

    Values will not persist in dictionary - unable to fix at the moment

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -4,25 +4,21 @@ const listOrder = [];
 const dictValue = {};
 const request = require('request');
 
-function urlAssociation (eachUrl) {
-  request.get(eachUrl, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    }
-    dictValue[eachUrl] = JSON.parse(body).name;
-  });
-}
-
 request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   }
   const characterURL = JSON.parse(body).characters;
+  listOrder.push(characterURL);
   characterURL.forEach(function (eachUrl) {
-    listOrder.push(eachUrl);
-    urlAssociation(eachUrl);
+    request.get(eachUrl, function (err, response, body) {
+      if (err) {
+        console.log(err);
+      }
+      dictValue[eachUrl] = JSON.parse(body).name;
+    });
   });
-});
-listOrder.forEach(function (url) {
-  console.log(dictValue[url]);
+  listOrder.forEach(function (url) {
+    console.log(dictValue[url]);
+  });
 });
