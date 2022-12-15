11dda6fc1959216e73d1b4e9c39aa5101fc0079e
Gotmished <5002@holbertonstudents.com>
2022-12-15 15:46:52 +1100

    Now querySelector works as soon as page DOM has loaded, rather than once everythin has loaded

--- javascript-web_jquery/100-script.js
+++ javascript-web_jquery/100-script.js
@@ -1,3 +1,10 @@
-window.onload = function () {
+/* window.onload = function () {
   document.querySelector('header').style.color = '#FF0000';
 };
+Works upon loading the whole webpage */
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
+/* Works as soon as the page DOM has loaded, without waiting
+for resources to finish loading */
